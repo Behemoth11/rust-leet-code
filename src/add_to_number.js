f8f46aef389eff4ac7d2e15ab05bec5da4e59dd1
Behemoth11 <kodohdaniel@gmail.com>
2022-04-05 23:09:42 -0500

    improved add_two_numbers implementation, not going to implement in rust ( yet )

--- src/add_to_number.js
+++ src/add_to_number.js
@@ -12,45 +12,52 @@
  */
  var addTwoNumbers = function(l1, l2) {
     
-    let head = l1;
-    
-    while (l1){
-        if (!l2) {
-            l2 = {val: 0, next: null}
-        }
-        
-        let carry = 0;
-        let node_addition = l1.val + l2.val;
-        
-        if ( node_addition >= 10 ){
-            node_addition = node_addition % 10;
-            carry = 1;
-        }
-        
-        l1.val = node_addition;
-        
-        if (l1.next) l1.next.val += carry;
-        
-        else {
-            if (carry == 1 ){
-                l1.next = {
-                    val: 1,
-                    next: null
-                }
-            }
-            else {
-                l1.next = l2.next;
-                break;
-            }
-        }
-        
-        
-        l1 = l1.next;
-        l2 = l2.next;
-        
-    }
-    
-    
-    
-    return head;
+    let carry = 0;
+    let head = {val: 0, next: null};
+    let current = head;
+
+   while ( l1 || l2 ){
+       let val1 = l1?.val ? l1.val : 0;
+       let val2 = l2?.val ? l2.val : 0;
+       
+       let total = val1 + val2 + carry;     
+       carry = Math.floor(total/10);
+       
+       current.next = l1 || l2;
+       current.next.val = total - carry * 10;
+       
+       current = current.next;
+       l1 = l1?.next;
+       l2 = l2?.next;
+   }
+   
+   if (carry > 0){
+       current.next = {val: 1, next: null}
+   }
+   
+   return head.next;
 };
+
+const createListNode = function ( arr){
+
+    let head = {val: 0, next: null};
+
+    let current = head;
+
+    arr.forEach(num => {
+        current.next = { val: num, next: null };
+        current = current.next;
+    })
+
+    return head;
+}
+
+
+
+
+const node_1 = createListNode([2,4,3]);
+const node_2 = createListNode([5,6,4]);
+
+console.log(4)
+console.log(addTwoNumbers(node_1, node_2))
+
